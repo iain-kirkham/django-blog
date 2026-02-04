71da9b4f10312943db3d77e29939ecf970319c10
Iain <iain.kirkham@outlook.com>
2026-02-04 15:20:53 +0000

    feat: change post view to only show pulished posts

--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,5 @@ from django.views import generic
 from .models import Post
 
 class PostList(generic.ListView):
-    queryset = Post.objects.all()
+    queryset = Post.objects.filter(status=1)
     template_name = "post_list.html"
